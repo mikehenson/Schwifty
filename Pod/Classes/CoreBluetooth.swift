f8d5e6dc124b370004386bbd9800f32422984607
Michael Henson <mike@inscrutableape.com>
2015-12-30 11:09:02 -0800

    Add CoreBluetooth error enums

--- Pod/Classes/CoreBluetooth.swift
+++ Pod/Classes/CoreBluetooth.swift
@@ -7,6 +7,90 @@
 
 import CoreBluetooth
 
+extension CBError: CustomStringConvertible, CustomDebugStringConvertible {
+    public var description: String {
+        var stateText = ""
+        switch self {
+        case .Unknown:
+            stateText = "Unknown"
+        case .InvalidParameters:
+            stateText = "Invalid Parameters"
+        case .InvalidHandle:
+            stateText = "Invalid Handle"
+        case .NotConnected:
+            stateText = "Not Connected"
+        case .OutOfSpace:
+            stateText = "Out of Space"
+        case .OperationCancelled:
+            stateText = "Operation Cancelled"
+        case .ConnectionTimeout:
+            stateText = "Connection Timeout"
+        case .PeripheralDisconnected:
+            stateText = "Peripheral Disconnected"
+        case .UUIDNotAllowed:
+            stateText = "UUID Not Allowed"
+        case .AlreadyAdvertising:
+            stateText = "Already Advertising"
+        case .ConnectionFailed:
+            stateText = "Connection Failed"
+        case .ConnectionLimitReached:
+            stateText = "Connection Limit Reached"
+        }
+        return stateText
+    }
+    public var debugDescription: String {
+        return self.description
+    }
+}
+
+extension CBATTError: CustomStringConvertible, CustomDebugStringConvertible {
+    public var description: String {
+        var stateText = ""
+        switch self {
+        case .Success:
+            stateText = "Success"
+        case .InvalidHandle:
+            stateText = "Invalid Handle"
+        case .ReadNotPermitted:
+            stateText = "Read Not Permitted"
+        case .WriteNotPermitted:
+            stateText = "Write Not Permitted"
+        case .InvalidPdu:
+            stateText = "Invalid Pdu"
+        case .InsufficientAuthentication:
+            stateText = "Insufficient Authentication"
+        case .RequestNotSupported:
+            stateText = "Request Not Supported"
+        case .InvalidOffset:
+            stateText = "Invalid Offset"
+        case .InsufficientAuthorization:
+            stateText = "Insufficient Authorization"
+        case .PrepareQueueFull:
+            stateText = "Prepare Queue Full"
+        case .AttributeNotFound:
+            stateText = "Attribute Not Found"
+        case .AttributeNotLong:
+            stateText = "Attribute Not Long"
+        case .InsufficientEncryptionKeySize:
+            stateText = "Insufficient Encryption Key Size"
+        case .InvalidAttributeValueLength:
+            stateText = "Invalid Attribute Value Length"
+        case .UnlikelyError:
+            stateText = "Unlikely Error"
+        case .InsufficientEncryption:
+            stateText = "Insufficient Encryption"
+        case .UnsupportedGroupType:
+            stateText = "Unsupported Group Type"
+        case .InsufficientResources:
+            stateText = "Insufficient Resources"
+        }
+        return stateText
+    }
+    public var debugDescription: String {
+        return self.description
+    }
+}
+
 extension CBCentralManagerState: CustomStringConvertible, CustomDebugStringConvertible {
     public var description: String {
         var stateText = ""
